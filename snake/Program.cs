7c1964bcc4265da45ded24ec2b73b9fdfe50cc4d
Vanechello <vanechello@mail.ru>
2016-05-08 16:31:59 +0300

    Координаты точек lesson 2

--- snake/Program.cs
+++ snake/Program.cs
@@ -9,8 +9,24 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!!");
-            Console.ReadLine();
+            int x1 = 3;
+            int y1 = 1;
+            char sim1 = '*';
+            Draw(x1,y1,sim1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sim2 = '*';
+             Draw(x2,y2,sim2);
+
+          
+           
         }
+        static void Draw(int x, int y, char sim)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sim);
+        }
+        Console.ReadLine();
     }
-}
+
